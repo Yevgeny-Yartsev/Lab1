923f9dabc5ef78112e9e7879fab7df6c2f43704d
Yevgeny Yartsev <ZenchikNic@yandex.ru>
2020-02-29 11:28:27 +0700

    Fix Chessviz

--- Chessviz.c
+++ Chessviz.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-
 int main()
 {
-    char const K = 'K'; 
-    char const F = 'F'; 
-    char const S = 'S'; 
-    char const H = 'H'; 
-    char const L = 'L'; 
-    char const P = 'P'; 
+    char const K = 'K';
+    char const F = 'F';
+    char const S = 'S';
+    char const H = 'H';
+    char const L = 'L';
+    char const P = 'P';
 
     char Pole[9][9] = {
             {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
@@ -22,8 +21,7 @@ int main()
             {'8', S, L, H, K, F, H, L, S},
     };
 
-    for (int i = 0; i < 9; i++)
-    {
+    for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++)
             printf("%c", Pole[i][j]);
         printf("\n");
